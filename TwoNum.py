9f49484047bb15ac94b19228c0629fe5c219c130
xiangping.hu <xiangping.hu@17zuoye.com>
2019-07-11 04:38:28 +0800

    两数之和

---
+++ TwoNum.py
@@ -0,0 +1,25 @@
+# 1
+# S
+# 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+# 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+# 示例:
+#	给定 nums = [2, 7, 11, 15], target = 9
+#	因为 nums[0] + nums[1] = 2 + 7 = 9
+#	所以返回 [0, 1]
+
+class Solution(object):
+	def twoSum(self, nums, target):
+		lenNums = len(nums)
+		if not lenNums:
+			return None
+		j = 0
+		for i in range(lenNums - 1):
+			j = i + 1
+			while j < lenNums:
+				if nums[i] + nums[j] == target:
+					return [i, j]
+				j = j + 1
+
+s = Solution()
+a = s.twoSum([0, 2, 1], 3)
+print(a)
